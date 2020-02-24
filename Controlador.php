fdabc48a9595f5e8d66534d8608b75f6e1bc275b
ANGEL_PAGOLA <angelpagolapajuelo99@gmail.com>
2020-02-23 23:18:02 -0500

    2.2 commit: Agregamos el metodo cabeceraTabla y LlenarTabla

--- Controlador.php
+++ Controlador.php
@@ -8,3 +8,37 @@ function  DevolverRegistroDato($consulta)
     $resultset = mysqli_query($Base->Conexion(), $consulta);
     return $resultset;
 }
+
+function Cabecera($cabecera)
+{
+    echo ("<div class='container'>"
+        . "<div class='row'>"
+        . "<div class='col-lg-11'>"
+        . "<div class='table-responsive'>"
+        . "<table class='table table-striped table-bordered' id='' style='width:100%'>"
+        . "<thead>"
+        . "<tr>");
+    for ($i = 0; $i < count($cabecera); $i++) {
+        echo  "<th style='padding:4px; text-align:center; font-size:14px; border-top: 1px solid #dddddd;'>$cabecera[$i]</th>";
+    }
+    echo ("</thead>"
+        . "<tbody>");
+}
+
+function LenarTabla($consulta, $arreglo, $longitud)
+{
+    $registro = DevolverRegistroDato($consulta);
+    Cabecera($arreglo);
+
+    while ($fila = mysqli_fetch_row($registro)) {
+        echo "<tr>";
+        for ($i = 0; $i < $longitud; $i++) {
+            echo ("<td style='text-align:center; font-size:14px;'>" . $fila[$i] . "</td>");
+        }
+        echo "</tr>";
+    }
+    echo ("</tbody>"
+        . "</table>" . "</div>" . "</div>"
+        . "</div>" . "</div>");
+}
+
